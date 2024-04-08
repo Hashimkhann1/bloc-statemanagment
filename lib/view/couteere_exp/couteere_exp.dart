cd0b5db5c230d07226467bcf7bc7f454caac8b87
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-04-08 23:51:19 +0500

    this file add increment and decrement with bloc

--- lib/view/couteere_exp/couteere_exp.dart
+++ lib/view/couteere_exp/couteere_exp.dart
@@ -1,10 +1,15 @@
+import 'package:blockstatemanagment/view_model/bloc/counter_bloc/counter_bloc.dart';
+import 'package:blockstatemanagment/view_model/bloc/counter_bloc/counter_event.dart';
+import 'package:blockstatemanagment/view_model/bloc/counter_bloc/counter_state.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CounterExp extends StatelessWidget {
   const CounterExp({super.key});
 
   @override
   Widget build(BuildContext context) {
+    print('build>>>>');
     return Scaffold(
       appBar: AppBar(
         title: Text("Counter Exfample"),
@@ -14,33 +19,33 @@ class CounterExp extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
-              '0',
-              style: TextStyle(fontSize: 40),
-            ),
+            BlocBuilder<CounterBloc , CounterState>(builder: (context , state) {
+              return Text(
+                state.counter.toString(),
+                style: TextStyle(fontSize: 40),
+              );
+            }),
             SizedBox(height: 20,),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                TextButton(
-                  onPressed: () {},
-                  style: TextButton.styleFrom(
-                    backgroundColor: Colors.black38
-                  ),
+                ElevatedButton(
+                  onPressed: () {
+                    context.read<CounterBloc>().add(IncrementCounter());
+                  },
                   child: Text(
                     'Increment',
-                    style: TextStyle(fontSize: 20,color: Colors.cyanAccent),
+                    style: TextStyle(fontSize: 20,),
                   ),
                 ),
                 SizedBox(width: 10,),
-                TextButton(
-                  onPressed: () {},
-                  style: TextButton.styleFrom(
-                    backgroundColor: Colors.black38
-                  ),
+                ElevatedButton(
+                  onPressed: () {
+                    context.read<CounterBloc>().add(DecrementCounter());
+                  },
                   child: Text(
                     'Dcrement',
-                    style: TextStyle(fontSize: 20,color: Colors.cyanAccent),
+                    style: TextStyle(fontSize: 20,),
                   ),
                 ),
               ],
