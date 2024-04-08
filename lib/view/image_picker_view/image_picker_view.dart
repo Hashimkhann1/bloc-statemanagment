cb9202c62955530022d867276d33d6110848d6a7
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-04-08 23:49:38 +0500

    this file show image picker

---
+++ lib/view/image_picker_view/image_picker_view.dart
@@ -0,0 +1,49 @@
+import 'dart:io';
+
+import 'package:blockstatemanagment/view_model/bloc/image_picker_bloc/image_picker_bloc.dart';
+import 'package:blockstatemanagment/view_model/bloc/image_picker_bloc/image_picker_event.dart';
+import 'package:blockstatemanagment/view_model/bloc/image_picker_bloc/image_picker_state.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class ImagePickerView extends StatelessWidget {
+  const ImagePickerView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    final height = MediaQuery.of(context).size.height;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Image Picker'),
+      ),
+      body: Column(
+        children: [
+          BlocBuilder<ImagePickerBloc , ImagePickerState>(builder: (context , state) {
+            return Container(
+              width: width,
+              height: height * 0.44,
+              alignment: Alignment.center,
+              child: state.file != null ? Image.file(File(state.file!.path.toString())) : Text(
+                'Image is not selected',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+              ),
+            );
+          }),
+          SizedBox(
+            height: height * 0.03,
+          ),
+          InkWell(
+            onTap: () {
+              context.read<ImagePickerBloc>().add(PickImageFromCamra());
+            },
+              child: Icon(
+            Icons.photo_camera_sharp,
+            size: 36,
+            color: Colors.black54,
+          ))
+        ],
+      ),
+    );
+  }
+}
