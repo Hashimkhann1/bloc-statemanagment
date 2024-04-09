5e5767d862e8a041fb999fe535ca7f66644b6a19
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-04-09 22:37:12 +0500

    compleetd

---
+++ lib/view_model/bloc/counter_bloc/counter_state.dart
@@ -0,0 +1,20 @@
+
+
+
+import 'package:equatable/equatable.dart';
+
+class CounterState extends Equatable {
+  final int counter;
+
+  const CounterState({this.counter = 1});
+
+  CounterState copyWith ({int? counter}) {
+    return CounterState(counter: counter ?? this.counter);
+  }
+
+  @override
+  List<Object?> get props => [counter] ;
+
+
+
+}
