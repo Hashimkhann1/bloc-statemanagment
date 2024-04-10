1c2f15b45c6b565393c108f72069444b866d86d6
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-04-10 21:33:15 +0500

    image with bloc is completed

---
+++ lib/view_model/bloc/image_picker_bloc/image_picker_bloc.dart
@@ -0,0 +1,25 @@
+
+
+
+import 'package:blockstatemanagment/utils/image_picker_utils.dart';
+import 'package:blockstatemanagment/view_model/bloc/image_picker_bloc/image_picker_event.dart';
+import 'package:blockstatemanagment/view_model/bloc/image_picker_bloc/image_picker_state.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:image_picker/image_picker.dart';
+
+class ImagePickerBloc extends Bloc<ImagePickerEvent , ImagePickerState> {
+
+  final ImagePickerUtils imagePickerUtils;
+
+  ImagePickerBloc(this.imagePickerUtils) : super(ImagePickerState()) {
+    on<PickImageFromCamra>(_pickImageFromCamra);
+  }
+
+  void _pickImageFromCamra(PickImageFromCamra event , Emitter emit) async {
+    XFile? file = await imagePickerUtils.camraImagePicker();
+    emit(state.copyWith(file: file));
+  }
+
+
+
+}
