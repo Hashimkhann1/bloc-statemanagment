96c367e630388bee72c5eb65e7473fe56ed27122
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-04-10 21:33:35 +0500

    image with bloc is completed

--- lib/view_model/bloc/image_picker_bloc/image_picker_event.dart
+++ lib/view_model/bloc/image_picker_bloc/image_picker_event.dart
@@ -2,10 +2,18 @@
 
 
 import 'package:equatable/equatable.dart';
+import 'package:image_picker/image_picker.dart';
 
-abstract class ImagePickerState extends Equatable {
+abstract class ImagePickerEvent extends Equatable {
+
+  ImagePickerEvent();
 
   @override
   List<Object> get props => [];
 
 }
+
+
+class PickImageFromCamra extends ImagePickerEvent{}
+
+class PickImageFromGallery extends ImagePickerEvent{}
