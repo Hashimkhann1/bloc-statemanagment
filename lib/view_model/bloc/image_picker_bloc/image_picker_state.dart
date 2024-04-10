30c9db7d21967d9de0f98ebe61cd6110229b6b74
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-04-10 21:34:11 +0500

    image with bloc is completed

---
+++ lib/view_model/bloc/image_picker_bloc/image_picker_state.dart
@@ -0,0 +1,18 @@
+
+
+import 'package:equatable/equatable.dart';
+import 'package:image_picker/image_picker.dart';
+
+class ImagePickerState extends Equatable{
+
+  final XFile? file;
+  ImagePickerState({this.file});
+
+  ImagePickerState copyWith({XFile? file}){
+    return ImagePickerState(file: file ?? this.file);
+  }
+
+  @override
+  List<Object?> get props => [file];
+
+}
