5decdd6510ee0e27b9d09bf05e0d320fc7af5576
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-04-10 21:35:02 +0500

    all example for bloc buttons

---
+++ lib/view/all_example_view/all_example_view.dart
@@ -0,0 +1,36 @@
+import 'package:blockstatemanagment/view/couteere_exp/couteere_exp.dart';
+import 'package:blockstatemanagment/view/exapmle_two/example_two.dart';
+import 'package:blockstatemanagment/view/image_picker_view/image_picker_view.dart';
+import 'package:flutter/material.dart';
+
+class AllExampleView extends StatelessWidget {
+  const AllExampleView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("All Example Buttons"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ElevatedButton(onPressed: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) => CounterExp()));
+            }, child: Text("Counter",style: TextStyle(fontSize: 20),)),
+            SizedBox(height: 10,),
+            ElevatedButton(onPressed: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) => ExampleTwo()));
+            }, child: Text("Slider and Switch",style: TextStyle(fontSize: 20),)),
+            SizedBox(height: 10,),
+            ElevatedButton(onPressed: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) => ImagePickerView()));
+            }, child: Text("Image Picker",style: TextStyle(fontSize: 20),)),
+          ],
+        ),
+      ),
+    );
+  }
+}
