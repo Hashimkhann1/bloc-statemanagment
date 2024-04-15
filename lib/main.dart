366005c8e338efdbd955da22f4d66890da05e002
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-04-15 22:57:14 +0500

    updated for favourite item bloc

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,12 @@ import 'package:blockstatemanagment/utils/image_picker_utils.dart';
 import 'package:blockstatemanagment/view/all_example_view/all_example_view.dart';
 import 'package:blockstatemanagment/view/exapmle_two/example_two.dart';
 import 'package:blockstatemanagment/view_model/bloc/counter_bloc/counter_bloc.dart';
+import 'package:blockstatemanagment/view_model/bloc/favourite_bloc/favourite_bloc.dart';
 import 'package:blockstatemanagment/view_model/bloc/image_picker_bloc/image_picker_bloc.dart';
 import 'package:blockstatemanagment/view_model/bloc/slider_bloc/slider_bloc.dart';
 import 'package:blockstatemanagment/view_model/bloc/switch_bloc/switch_bloc.dart';
 import 'package:blockstatemanagment/view_model/bloc/to_do_bloc/to_do_bloc.dart';
+import 'package:blockstatemanagment/view_model/create_fav_list/create_fav_list.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -25,12 +27,14 @@ class MyApp extends StatelessWidget {
         BlocProvider(create: (_) => SliderBloc()),
         BlocProvider(create: (_) => SwitchBloc()),
         BlocProvider(create: (_) => ImagePickerBloc(ImagePickerUtils()),),
-        BlocProvider(create: (_) => ToDoBloc())
+        BlocProvider(create: (_) => ToDoBloc()),
+        BlocProvider(create: (_) => FavouriteBloc(CreateFavList())),
       ],
       child: MaterialApp(
           title: 'Flutter Demo',
+          themeMode: ThemeMode.dark,
           theme: ThemeData(
-            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+           brightness: Brightness.dark,
             useMaterial3: true,
           ),
           home: AllExampleView()
