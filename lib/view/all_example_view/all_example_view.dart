cc1c447044f38a88c8f5b629f7a7f914d271f7fc
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-04-15 22:55:54 +0500

    updated for favourite itme button

--- lib/view/all_example_view/all_example_view.dart
+++ lib/view/all_example_view/all_example_view.dart
@@ -1,5 +1,6 @@
 import 'package:blockstatemanagment/view/couteere_exp/couteere_exp.dart';
 import 'package:blockstatemanagment/view/exapmle_two/example_two.dart';
+import 'package:blockstatemanagment/view/favourite_view/favourite_view.dart';
 import 'package:blockstatemanagment/view/image_picker_view/image_picker_view.dart';
 import 'package:blockstatemanagment/view/to_do_view/to_do_view.dart';
 import 'package:flutter/material.dart';
@@ -33,6 +34,10 @@ class AllExampleView extends StatelessWidget {
             ElevatedButton(onPressed: () {
               Navigator.push(context, MaterialPageRoute(builder: (context) => ToDoView()));
             }, child: Text("To Do",style: TextStyle(fontSize: 20),)),
+            SizedBox(height: 10,),
+            ElevatedButton(onPressed: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) => FavouriteView()));
+            }, child: Text("Favourite",style: TextStyle(fontSize: 20),)),
           ],
         ),
       ),
